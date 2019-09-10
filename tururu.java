444f20e87c144729418245395e8f02f0379e7c82
Karina Flores <karinaflores769@gmail.com>
2019-09-10 10:30:45 -0500

    segunda sincronizacion

--- tururu.java
+++ tururu.java
@@ -1,7 +1,7 @@
 public class void tururu(){
 	public static void main(String[] args){
 		System.out.println("TAPIA SANDOVAL EDUARDO");
-
+		System.out.println("FLORES CRUZ KARINA ALEJANDRA");
 	}
 
 }
