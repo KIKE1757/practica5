0def0c171413b31c81420746b9c0a69c2f8282eb
Velazquez Rodriguez Diego <poo02alu40@Angola04.local>
2019-09-10 10:36:45 -0500

    Tercer sincronizacion

--- tururu.java
+++ tururu.java
@@ -2,6 +2,7 @@ public class void tururu(){
 	public static void main(String[] args){
 		System.out.println("TAPIA SANDOVAL EDUARDO");
 		System.out.println("FLORES CRUZ KARINA ALEJANDRA");
+		System.out.println("VELAZQUEZ RODRIGUEZ DIEGO");
 	}
 
 }
