eb247f054cada8bfdac5b7a3357a447077143c4f
DiegoVelazquez1005 <55148451+DiegoVelazquez1005@users.noreply.github.com>
2019-09-14 18:48:35 -0500

    commit 5
    creacion de las clases

---
+++ Practica5POO/src/practica5poo/Practica5POO.java
@@ -0,0 +1,12 @@
+package practica5poo;
+
+public class Practica5POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
