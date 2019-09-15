8bbcd2eaab78e3c770ad9e1720c9682c853858c8
KIKE1757 <enriquevazquez5700@gmail.com>
2019-09-15 18:10:28 -0500

    Update cono.java

--- Practica5POO/src/practica5poo/cono.java
+++ Practica5POO/src/practica5poo/cono.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practica5poo;
-
-/**
- *
- * @author velaz
- */
-public class cono {
-    
+package cono;
+import java.util.Scanner;
+public class Cono {
+    float pi=3.14159265f;
+    private float g;
+    private float r;
+    private float h;
+    public Cono(float g,float r,float h){
+        this.g=g;
+        this.r=r;
+        this.h=h;
+}
+    public void volumen(){
+        float vol=(pi*r*r*h)/3;
+        System.out.println("El volumen del cono es: "+vol);
+    }
+    public void area(){
+        float area=pi*r*(r+g);
+        System.out.println("El volumen del cono es: "+area);
+    }
+    public void print(){
+        
+    }
+    public static void main(String[] args) {
+        Cono cono1= new Cono(45,12,40);
+        cono1.volumen();
+        cono1.area();
+    }
 }
