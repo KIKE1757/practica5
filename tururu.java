cff443c2b5ca4cbcca2c4ea4fc45f34f64a28da2
DiegoVelazquez1005 <55148451+DiegoVelazquez1005@users.noreply.github.com>
2019-09-15 15:44:03 -0500

    Update tururu.java

--- tururu.java
+++ tururu.java
@@ -1,5 +1,6 @@
 public class void tururu(){
 	public static void main(String[] args){
+		System.out.println("Equipo 11");
 		System.out.println("TAPIA SANDOVAL EDUARDO");
 		System.out.println("FLORES CRUZ KARINA ALEJANDRA");
 		System.out.println("VELAZQUEZ RODRIGUEZ DIEGO");
